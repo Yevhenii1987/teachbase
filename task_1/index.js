4803358c797b66a7a432f100bb39bf65fbde9091
Yevhenii1987 <yevhenii.bilousov@gotoinc.co>
2021-08-10 13:42:38 +0300

    task 1: add error

--- task_1/index.js
+++ task_1/index.js
@@ -1,6 +1,10 @@
 function renderData(element, dataObj) {
-  for (let i = 0; i < element.children.length; i++) {
-    element.children[i].textContent = dataObj[element.children[i].dataset.field];
+  for ( let i = 0; i < element.children.length; i++ ) {
+    if ( dataObj.hasOwnProperty(element.children[i].dataset.field) ) {
+      element.children[i].textContent = dataObj[element.children[i].dataset.field];
+    } else {
+      throw new Error(`${element.children[i].dataset.field} property not found`);
+    }
   }
 }
 
@@ -9,6 +13,7 @@ const el = document.getElementById('item');
 const data = {
   title: 'Hello JS',
   description: 'Everything will be Javascript',
+  email: 'mail@example.com'
 }
 
 renderData(el, data);
